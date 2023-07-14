d3eb60d0ef852c7f966df0e88f6df998f4b9c72e
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-07-14 10:57:33 -0300

    feat: start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.module.css'
+import { Header } from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Header />
+  )
+}
+
+export default App
