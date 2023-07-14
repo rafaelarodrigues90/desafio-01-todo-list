3add7e09881ef81636384929d044c3abfb26605c
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-07-14 13:50:43 -0300

    refactor: ajuste comentarios

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -2,8 +2,8 @@ import styles from './styles.module.css';
 
 export function Info () {
     return (
+        // TODO: contadores
         <div className={styles.container}>
-            // TODO: contadores
             <strong className={styles.tarefas}>Tarefas criadas</strong>
             <strong className={styles.concluidas}>Concluídas</strong>
         </div> 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,11 +3,11 @@ import styles from './styles.module.css';
 
 export function TodoList () {
     return (
+        // TODO: tasks container
         <div className={styles.container}>
             <div className={styles.taskContainer}>
                 <Info />
                 
-                // TODO: tasks container
             </div>
         </div>
     )
