920419bc1af600b9c9667c66bdf08f9107aafd56
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-07-18 18:45:55 -0300

    feat: header, input, functions and styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,35 @@
-import styles from './styles.module.css'
-import PlusIcon from '../../assets/plusIcon.svg';
+import { useState } from "react";
+import styles from "./styles.module.css";
+import PlusIcon from "../../assets/plusIcon.svg";
 
-export function Input() {
-    return(
-        <div className={styles.container}>
-            <div className={styles.inputContainer}>
-                <input className={styles.input} placeholder='Adicione uma nova tarefa' />
-                <button className={styles.button}>
-                    <strong className={styles.buttonText}>Criar</strong>
-                    <img src={PlusIcon} />
-                </button>
-            </div>
-        </div>
-    )
+interface InputProps {
+  onAddTask: (task: string) => void;
+}
+
+export function Input({ onAddTask }: InputProps) {
+  const [newTask, setNewTask] = useState<string>("");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      onAddTask(newTask);
+      setNewTask("");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.input}
+          placeholder="Adicione uma nova tarefa"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className={styles.button} onClick={handleAddTask}>
+          <strong className={styles.buttonText}>Criar</strong>
+          <img src={PlusIcon} alt="Plus Icon" />
+        </button>
+      </div>
+    </div>
+  );
 }
